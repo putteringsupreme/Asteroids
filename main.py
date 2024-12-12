097fa7a6e33c17c4f0cfa138c23fab64febe4ab6
putteringsupreme <seanronchetti@gmail.com>
2024-12-12 14:29:14 -0600

    added asteroids and asteroid field to spawn them

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame #type:ignore
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -11,10 +13,17 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) #set to 1280/720
     clock = pygame.time.Clock()
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroid_field = AsteroidField()
     dt = 0
 
     while True:
@@ -22,11 +31,13 @@ def main():
             if event.type == pygame.QUIT:
                 return
     
+        for obj in updatable:
+            obj.update(dt)
+        
         screen.fill((0,0,0))
         
-        player.draw(screen) #draws the player sprite on the screen, starts at center
-        
-        player.update(dt)
+        for sprite in drawable:
+            sprite.draw(screen) #draws the player sprite on the screen, starts at center
 
         pygame.display.flip()
 
